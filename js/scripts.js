aec4a41654d2e569113635829cb8f477b285cd97
Wei <weinini007@gmail.com>
2018-03-26 16:10:48 +0200

    Add consts

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,9 @@
 const cards = {1: "santa", 2: "hotel", 3: "ice", 4: "hospital", 5:"church", 6:"degree",
                 7:"inspiration", 8:"camp"};
 
-let firstCard = 0;
+const cardFront = document.querySelectorAll(".card-front");
+const cardBack = document.querySelectorAll(".card-back");
+const star = document.querySelectorAll(".fa-star");
+const winner = document.querySelector(".win-popup");
+let seconds, minutes;
+            
